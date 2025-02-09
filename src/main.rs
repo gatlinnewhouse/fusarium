8dcfb5dfadb4a4a4359b71f39a4179cdd3370137
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-08 17:35:18 -0800

    VGA buffers with print macros added!
    What I learned:
    * The color codes I found online before the blog did colors were similar
      but the names of colors were slightly different. So I anticipated
      that.
    * Volatile is a crate that provides volatile memory access and ensures
      the compiler never optimizes away this access
    * Lazy static is a crate allowing us to execute static types at runtime
      so we avoid compiler errors that require more information about
      initialized values
    * Spin is a crate implementing synchronization primitives as spinlocks

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[allow(clippy::empty_loop)]
     loop {}
 }
