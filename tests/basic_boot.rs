ac19573d60eea801df355e2be376b85683976587
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-08 18:42:45 -0800

    Integration tests working
    I did not realize that the
    
    ```rust
    test_main();
    ```
    
    that I hastily removed was required for my integration tests to work.

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,31 +9,18 @@ use fusarium::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     #[allow(clippy::empty_loop)]
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    #[allow(clippy::empty_loop)]
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     fusarium::test_panic_handler(info)
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
