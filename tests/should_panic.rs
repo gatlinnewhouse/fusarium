b0d1c1f13409bcec582405de3782f988c040c1df
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-08 18:47:49 -0800

    Should panic integrations without overhead
    What I learned
    * The harness attribute in Cargo tests can be used for tests that
      require a certain runtime order due to side affects, and it allows us
      to go without a test_runner function

---
+++ tests/should_panic.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use fusarium::{exit_qemu, serial_print, serial_println, QemuExitCode};
+
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
