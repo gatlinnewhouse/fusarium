6db960bfd685198cfdd75d624172060940afc3fb
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-23 02:09:37 -0800

    Create pages from offsets and frames

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,30 @@
 #![test_runner(fusarium::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use fusarium::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+entry_point!(kernel_main);
 
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use fusarium::memory;
+    #[cfg(target_arch = "x86_64")]
+    use x86_64::{structures::paging::Page, VirtAddr};
+    println!("Hello World{}", "!");
     fusarium::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = memory::init(phys_mem_offset);
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
