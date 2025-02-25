d1ac8bf50cddd2d3b70aacae303d7f70798b0328
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-25 00:25:06 -0800

    Bump allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,3 +1,5 @@
+use bump::BumpAllocator;
+#[cfg(feature = "alloc-linked-list")]
 use linked_list_allocator::LockedHeap;
 #[cfg(target_arch = "x86_64")]
 use x86_64::{
@@ -9,8 +11,30 @@ use x86_64::{
 
 pub mod bump;
 
+/// A wrapper around spin::Mutex to permit trait implementations.
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+#[cfg(feature = "alloc-linked-list")]
 #[global_allocator]
 static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+#[cfg(feature = "alloc-bump")]
+#[global_allocator]
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -42,4 +66,11 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+/// Align the given address `addr` upwards to alignment `align`.
+///
+/// Requires that `align` is a power of two.
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
 }
--- src/allocator/bump.rs
+++ src/allocator/bump.rs
@@ -1,4 +1,8 @@
-use core::alloc::{GlobalAlloc, Layout};
+use super::{align_up, Locked};
+use core::{
+    alloc::{GlobalAlloc, Layout},
+    ptr,
+};
 
 pub struct BumpAllocator {
     heap_start: usize,
@@ -29,12 +33,31 @@ impl BumpAllocator {
     }
 }
 
-unsafe impl GlobalAlloc for BumpAllocator {
+unsafe impl GlobalAlloc for Locked<BumpAllocator> {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let alloc_start = self.next;
-        self.next = alloc_start + layout.size();
-        self.allocations += 1;
-        alloc_start as *mut u8
+        let mut bump = self.lock(); // get a mutable reference
+
+        let alloc_start = align_up(bump.next, layout.align());
+        let alloc_end = match alloc_start.checked_add(layout.size()) {
+            Some(end) => end,
+            None => return ptr::null_mut(),
+        };
+
+        if alloc_end > bump.heap_end {
+            ptr::null_mut() // out of memory
+        } else {
+            bump.next = alloc_end;
+            bump.allocations += 1;
+            alloc_start as *mut u8
+        }
     }
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {}
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        let mut bump = self.lock(); // get a mutable reference
+
+        bump.allocations -= 1;
+        if bump.allocations == 0 {
+            bump.next = bump.heap_start;
+        }
+    }
 }
