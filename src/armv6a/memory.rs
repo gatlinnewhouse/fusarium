4972a4cbd183336c1d9ec54b362407e48c67a853
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-03-09 01:40:50 -0800

    Add attribution for registers

--- src/armv6a/memory.rs
+++ src/armv6a/memory.rs
@@ -1,3 +1,4 @@
+// From https://github.com/thanoskoutr/armOS/wiki/Raspberry-Pi-Hardware
 pub const MMIO_BASE: usize = 0x20000000;
 pub const GPIO_BASE: usize = 0x20200000;
 pub const UART_BASE: usize = 0x20201000;
